7a6ebf5587f6979c4eff9d3d324d19aa88971e54
YOG63752 <yog63752@educastur.es>
2026-05-12 18:19:49 +0200

    Se aaden suma, controlador y test unitario

---
+++ src/main/java/com/ciberGFS/Calculator.java
@@ -0,0 +1,9 @@
+package com.ciberGFS.calculatorGFS;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator{
+	int sum(int a, int b) {
+		return a + b;
+	}
+}
--- src/main/java/com/ciberGFS/CalculatorGfsApplication.java
+++ src/main/java/com/ciberGFS/CalculatorGfsApplication.java
@@ -1,4 +1,4 @@
-package calculatorGFS;
+package com.ciberGFS.calculatorGFS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
