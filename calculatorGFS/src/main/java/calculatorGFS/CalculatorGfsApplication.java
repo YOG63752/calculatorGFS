76c235bcf89116fcd4880868e6bc9c9366c4714a
YOG63752 <yog63752@educastur.es>
2026-05-12 17:10:36 +0200

    Añadido esqueleto Spring Boot

---
+++ calculatorGFS/src/main/java/calculatorGFS/CalculatorGfsApplication.java
@@ -0,0 +1,13 @@
+package calculatorGFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorGfsApplication.class, args);
+	}
+
+}
---
+++ calculatorGFS/src/test/java/calculatorGFS/CalculatorGfsApplicationTests.java
@@ -0,0 +1,13 @@
+package calculatorGFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
