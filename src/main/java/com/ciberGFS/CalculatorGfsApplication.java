c786da39d9ab37f8cb2b2da3f02a991c70056000
YOG63752 <yog63752@educastur.es>
2026-05-14 16:51:11 +0200

    Corregidos tabuladores

--- src/main/java/com/ciberGFS/CalculatorGfsApplication.java
+++ src/main/java/com/ciberGFS/CalculatorGfsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorGfsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorGfsApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(CalculatorGfsApplication.class, args);
+}
 
 }
